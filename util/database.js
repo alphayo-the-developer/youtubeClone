05648875e411ff4b1af71063d1f12d0f850f436a
alphayo-the-developer <alphayothedeveloper@gmail.com>
2021-12-29 02:00:15 +0300

    fix db

--- util/database.js
+++ util/database.js
@@ -3,35 +3,35 @@ const mongodb = require('mongodb');
 const mongoClient = mongodb.MongoClient;
 
 let db;
-
-// const mongoConnect = (callback) => {
-//   mongoClient.connect('mongodb://user1:manchester237@node-course-shard-00-00.hmo89.mongodb.net:27017,node-course-shard-00-01.hmo89.mongodb.net:27017,node-course-shard-00-02.hmo89.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-e2z2v5-shard-0&authSource=admin&retryWrites=true&w=majority')
-//   .then( client => {
-//     console.log('connected');
-//     _db = client.db();
-//     callback()
-//   })
-//   .catch( err => {
-//     console.log(err);
-//     throw err;
-//   })
-// }
-
-
-
+// mongodb://user1:manchester237@node-course-shard-00-00.hmo89.mongodb.net:27017,node-course-shard-00-01.hmo89.mongodb.net:27017,node-course-shard-00-02.hmo89.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-e2z2v5-shard-0&authSource=admin&retryWrites=true&w=majority
 const mongoConnect = (callback) => {
-  mongoClient.connect('mongodb://127.0.0.1:27017/', {useNewUrlParser:true})
+  mongoClient.connect('mongodb+srv://user1:manchester237@node-course.hmo89.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
   .then( client => {
-    db = client.db('youtube');
     console.log('connected');
+    _db = client.db();
     callback()
   })
   .catch( err => {
     console.log(err);
     throw err;
   })
-
 }
+
+
+
+// const mongoConnect = (callback) => {
+//   mongoClient.connect('mongodb://127.0.0.1:27017/', {useNewUrlParser:true})
+//   .then( client => {
+//     db = client.db('youtube');
+//     console.log('connected');
+//     callback()
+//   })
+//   .catch( err => {
+//     console.log(err);
+//     throw err;
+//   })
+
+// }
 
 
 const getDb = () => {
